30c0763d9a8a4be7230a830baff99237aec57dd4
Ingenieria <ingenieria@juacopan.loc>
2024-12-19 10:29:18 -0500

    actualización

--- pm.bat
+++ pm.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Cambiar directorio a la carpeta .pm2
-cd C:\Users\sistemas10\.pm2
+cd C:\Users\USUARIO\.pm2
 
 :: Restaurar procesos PM2
 pm2 resurrect
--- src/database/migrations/1730300123503-add-field-itemid.ts
+++ src/database/migrations/1730300123503-add-field-itemid.ts
@@ -1,17 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateFieldItemid1730151186628 implements MigrationInterface {
-    name = 'CreateFieldItemid1730151186628'
+export class AddFieldItemid1730300123503 implements MigrationInterface {
+    name = 'AddFieldItemid1730300123503'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`ingredients\` ADD UNIQUE INDEX \`IDX_a955029b22ff66ae9fef2e161f\` (\`name\`)`);
+        await queryRunner.query(`ALTER TABLE \`task_detail\` ADD \`item_id\` int NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`items\` DROP FOREIGN KEY \`FK_fb0b5b8bc408db666a8bf407661\``);
         await queryRunner.query(`ALTER TABLE \`items\` DROP FOREIGN KEY \`FK_2553dadf75310d9280d7d68e07f\``);
         await queryRunner.query(`ALTER TABLE \`items\` DROP COLUMN \`cuantity\``);
         await queryRunner.query(`ALTER TABLE \`items\` ADD \`cuantity\` float NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`items\` CHANGE \`product_id\` \`product_id\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`items\` CHANGE \`ingredient_id\` \`ingredient_id\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`products\` ADD UNIQUE INDEX \`IDX_4c9fb58de893725258746385e1\` (\`name\`)`);
         await queryRunner.query(`ALTER TABLE \`task_detail\` DROP FOREIGN KEY \`FK_c972242fe1a104fb445d6cd69ea\``);
         await queryRunner.query(`ALTER TABLE \`task_detail\` DROP FOREIGN KEY \`FK_8dd105cc331cd48cf857d822ab2\``);
         await queryRunner.query(`ALTER TABLE \`task_detail\` CHANGE \`ingredient_id\` \`ingredient_id\` int NULL`);
@@ -41,14 +40,13 @@ export class CreateFieldItemid1730151186628 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`task_detail\` CHANGE \`ingredient_id\` \`ingredient_id\` int NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`task_detail\` ADD CONSTRAINT \`FK_8dd105cc331cd48cf857d822ab2\` FOREIGN KEY (\`task_id\`) REFERENCES \`task\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`task_detail\` ADD CONSTRAINT \`FK_c972242fe1a104fb445d6cd69ea\` FOREIGN KEY (\`ingredient_id\`) REFERENCES \`ingredients\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`products\` DROP INDEX \`IDX_4c9fb58de893725258746385e1\``);
         await queryRunner.query(`ALTER TABLE \`items\` CHANGE \`ingredient_id\` \`ingredient_id\` int NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`items\` CHANGE \`product_id\` \`product_id\` int NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`items\` DROP COLUMN \`cuantity\``);
-        await queryRunner.query(`ALTER TABLE \`items\` ADD \`cuantity\` varchar(20) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`items\` ADD \`cuantity\` varchar(20) NOT NULL DEFAULT ''0''`);
         await queryRunner.query(`ALTER TABLE \`items\` ADD CONSTRAINT \`FK_2553dadf75310d9280d7d68e07f\` FOREIGN KEY (\`ingredient_id\`) REFERENCES \`ingredients\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`items\` ADD CONSTRAINT \`FK_fb0b5b8bc408db666a8bf407661\` FOREIGN KEY (\`product_id\`) REFERENCES \`products\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`ingredients\` DROP INDEX \`IDX_a955029b22ff66ae9fef2e161f\``);
+        await queryRunner.query(`ALTER TABLE \`task_detail\` DROP COLUMN \`item_id\``);
     }
 
 }
